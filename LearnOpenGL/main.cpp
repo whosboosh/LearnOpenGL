0d861d6cc401f376b74db39bf0709e8b8ddea82f
Nathanial Howard <nathanial55@gmail.com>
2021-03-07 17:58:13 +0000

    Okay back to basic framebuffers...

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -34,7 +34,7 @@ std::vector<Mesh*> meshList;
 
 Shader geometryShader;
 Shader directionalShadowShader;
-Shader antiAliasingShader;
+Shader screenShader;
 
 Camera camera;
 
@@ -43,8 +43,6 @@ Texture plainTexture;
 Texture brickDiffuse;
 Texture brickNormal;
 Texture brickDisplacement;
-
-MultiSampler multiSampler;
 
 Material shinyMaterial;
 Material dullMaterial;
@@ -69,7 +67,7 @@ float maxSize = 0.8f;
 float minSize = 0.1f;
 bool sizeDirection = false;
 
-
+unsigned int quadVAO, quadVBO;
 
 void CreateObjects() {
 	std::vector<Vertex> vertices = {
@@ -141,17 +139,6 @@ void CreateObjects() {
 		1, 2, 3
 	};
 
-	float quadVertices[] = {   // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
-	// positions   // texCoords
-	-1.0f,  1.0f,  0.0f, 1.0f,
-	-1.0f, -1.0f,  0.0f, 0.0f,
-	 1.0f, -1.0f,  1.0f, 0.0f,
-
-	-1.0f,  1.0f,  0.0f, 1.0f,
-	 1.0f, -1.0f,  1.0f, 0.0f,
-	 1.0f,  1.0f,  1.0f, 1.0f
-	};
-
 	calcAverageNormals(indices, &vertices, sizeof(indices) / sizeof(*indices));
 	calcAverageNormals(indices2, &vertices2, sizeof(indices2) / sizeof(*indices2));
 	//calcAverageNormals(floorIndices, &floorVertices, sizeof(floorIndices) / sizeof(*floorIndices));
@@ -168,9 +155,27 @@ void CreateObjects() {
 	obj3->CreateMeshIndex(&floorVertices, floorIndices, sizeof(floorIndices) / sizeof(*floorIndices));
 	meshList.push_back(obj3);
 
-	Mesh* quadObject = new Mesh();
-	quadObject->CreateMesh(quadVertices, (sizeof(quadVertices) / sizeof(*quadVertices)) / 4);
-	meshList.push_back(quadObject);
+	float quadVertices[] = {   // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+	// positions   // texCoords
+	-1.0f,  1.0f,  0.0f, 1.0f,
+	-1.0f, -1.0f,  0.0f, 0.0f,
+	 1.0f, -1.0f,  1.0f, 0.0f,
+
+	-1.0f,  1.0f,  0.0f, 1.0f,
+	 1.0f, -1.0f,  1.0f, 0.0f,
+	 1.0f,  1.0f,  1.0f, 1.0f
+	};
+
+
+	glGenVertexArrays(1, &quadVAO);
+	glGenBuffers(1, &quadVBO);
+	glBindVertexArray(quadVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 
 }
 
@@ -179,7 +184,7 @@ void CreateShaders()
 	// Shaders
 	geometryShader.CreateFromFiles("Shaders/geometry_shader.vert", "Shaders/geometry_shader.frag");
 	directionalShadowShader.CreateFromFiles("Shaders/directional_shadow_map.vert", "Shaders/directional_shadow_map.frag");
-	antiAliasingShader.CreateFromFiles("Shaders/anti_aliasing.vert", "Shaders/anti_aliasing.frag");
+	screenShader.CreateFromFiles("Shaders/anti_aliasing.vert", "Shaders/anti_aliasing.frag");
 }
 
 void ComputePositionOffsets(float& fXOffset, float& fZOffset)
@@ -313,9 +318,36 @@ int main()
 	brickDisplacement = Texture("Textures/bricks2_disp.jpg");
 	brickDisplacement.LoadTexture();
 
-	multiSampler = MultiSampler(mainWindow.getBufferWidth(), mainWindow.getBufferHeight());
-	multiSampler.init();
-	antiAliasingShader.setInt("screenTexture", 3);
+
+	// shader configuration
+	// --------------------
+	screenShader.UseShader();
+	screenShader.setInt("screenTexture", 0);
+
+	// framebuffer configuration
+	// -------------------------
+	unsigned int framebuffer;
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+	// create a color attachment texture
+	unsigned int textureColorbuffer;
+	glGenTextures(1, &textureColorbuffer);
+	glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, mainWindow.getBufferWidth(), mainWindow.getBufferHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+	// create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+	unsigned int rbo;
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, mainWindow.getBufferWidth(), mainWindow.getBufferHeight()); // use a single renderbuffer object for both a depth AND stencil buffer.
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+	// now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 
 	shinyMaterial = Material(1.0f, 128);
 	dullMaterial = Material(0.3f, 4);
@@ -354,11 +386,15 @@ int main()
 		camera.keyControl(mainWindow.getKeys(), deltaTime);
 		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
 
-		// Clear window
-		multiSampler.BindFrameBuffer();
-		glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+		// render
+		// ------
+		// bind to framebuffer and draw scene as we normally would to color texture 
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
+
+		// make sure we clear the framebuffer's content
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glEnable(GL_DEPTH_TEST);
 
 		ComputePositionOffsets(xOffset, zOffset);
 		// Red, Green, Blue, ambientIntensity, diffuseIntensity, Pos(XYZ), 
@@ -367,16 +403,17 @@ int main()
 		//DirectionalShadowMapPass(&mainLight);
 		RenderPass(projection, camera.calculateViewMatrix()); // Draw to GBuffer FrameBuffer
 
-		
+		// now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+		// clear all relevant buffers
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessary actually, since we won't be able to see behind the quad anyways)
 		glClear(GL_COLOR_BUFFER_BIT);
-		glDisable(GL_DEPTH_TEST);
 
-		antiAliasingShader.UseShader();
-		multiSampler.Read(GL_TEXTURE3);
-		meshList[3]->RenderMesh();	
-		
+		screenShader.UseShader();
+		glBindVertexArray(quadVAO);
+		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);	// use the color attachment texture as the texture of the quad plane
+		glDrawArrays(GL_TRIANGLES, 0, 6);		
 
 		mainWindow.swapBuffers();
 	}
